6b83e6ce03de7a41ca42bb4c19669b1f0018a472
pkstDev <William27060827@hotmail.com>
2022-05-14 20:28:04 +0800

    Initial commit & release

---
+++ src/main/java/io/github/fabricistooeasy/installer/Main.java
@@ -0,0 +1,7 @@
+package io.github.fabricistooeasy.installer;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainGUI().setVisible(true);
+    }
+}
---
+++ src/main/java/io/github/fabricistooeasy/installer/MainGUI.java
@@ -0,0 +1,116 @@
+package io.github.fabricistooeasy.installer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Locale;
+
+public class MainGUI extends JFrame {
+
+    public MainGUI() throws HeadlessException {
+        super("Fabric Loader Installer");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(getOwner());
+        setSize(500, 200);
+        setLayout(new FlowLayout());
+        JPanel gameVersionPanel = new JPanel();
+        gameVersionPanel.add(new JLabel("Game Version: 1.6.4-MITE"));
+        add(gameVersionPanel, BorderLayout.NORTH);
+        JPanel loaderVersionPanel = new JPanel();
+        loaderVersionPanel.add(new JLabel("Fabric Loader Version:"));
+        JComboBox<String> loaderVersionCombo = new JComboBox<>();
+        new Thread(() -> {
+            String xml = readString("https://maven.fabricmc.net/net/fabricmc/fabric-loader/maven-metadata.xml");
+            String allLoaderVersion = xml.substring(xml.indexOf("<versions>") + "<versions>".length(), xml.lastIndexOf("</versions>")).replaceAll("\\s", "");
+            String[] split = allLoaderVersion.split("(<version>|</version>)");
+            for (int i = split.length - 1; i >= 0; i--) {
+                if (!split[i].equals("")) {
+                    loaderVersionCombo.addItem(split[i]);
+                }
+            }
+        }).start();
+        loaderVersionPanel.add(loaderVersionCombo);
+        add(loaderVersionPanel);
+        JPanel path = new JPanel();
+        path.add(new JLabel("Path"));
+        path.add(new JTextField(getMinecraftDir().getAbsolutePath()));
+        add(path, BorderLayout.CENTER);
+        JButton install = new JButton("Install");
+        install.addActionListener(e -> {
+            Object loaderVersion = loaderVersionCombo.getSelectedItem();
+            if (loaderVersion == null) {
+                JOptionPane.showMessageDialog(null, "please wait");
+            } else {
+                try {
+
+                    String name = String.format("fabric-loader-%s-1.6.4-MITE", loaderVersion);
+                    File dir = new File(getMinecraftDir(), "versions" + "/" + name);
+                    if (!dir.exists()) {
+                        Files.createDirectories(dir.toPath());
+                    }
+
+                    String string = getString(this.getClass().getResourceAsStream("/config.json"));
+                    string = string.replace("${loaderVersion}", loaderVersion.toString());
+                    Files.write(new File(dir, name + ".json").toPath(), string.getBytes(StandardCharsets.UTF_8));
+                    JOptionPane.showMessageDialog(null, "Fabric Loader for 1.6.4 MITE has been successfully installed into your game directory.\nYou might need to get a copy of MITE R196 in your game directory as well.");
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                    JOptionPane.showMessageDialog(null, exception);
+                }
+            }
+        });
+        add(install);
+    }
+
+    private File getMinecraftDir() {
+        File minecraftFolder;
+        if (getOsName().contains("win")) {
+            minecraftFolder = new File(System.getenv("APPDATA"), File.separator + ".minecraft");
+        } else if (getOsName().contains("mac")) {
+            minecraftFolder = new File(System.getProperty("user.home"), File.separator + "Library" + File.separator + "Application Support" + File.separator + "minecraft");
+        } else {
+            minecraftFolder = new File(System.getProperty("user.home"), File.separator + ".minecraft");
+        }
+        return minecraftFolder;
+    }
+
+    private String getOsName() {
+        return System.getProperty("os.name").toLowerCase(Locale.ROOT);
+    }
+
+    private String readString(String link) {
+        try {
+            URL url = new URL(link);
+            URLConnection urlConnection = url.openConnection();
+            HttpURLConnection connection = null;
+            if (urlConnection instanceof HttpURLConnection) {
+                connection = (HttpURLConnection) urlConnection;
+            }
+
+            if (connection == null) {
+                throw new NullPointerException(String.format("Link: '%s' fail", link));
+            }
+
+            return getString(connection.getInputStream());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+
+    private String getString(InputStream inputStream) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
